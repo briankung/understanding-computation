ed222bfa3a6d10ee873f9e9732a3a98b8a6420d2
Brian Kung <brian@callmekung.com>
2018-05-31 11:51:02 -0500

    Some AST node tests

--- lib/ast_nodes.rb
+++ lib/ast_nodes.rb
@@ -1,17 +1,80 @@
 require 'ostruct'
 
 module ASTNodes
+  class Number < Struct.new(:value)
+    def to_s
+      value.to_s
+    end
+
+    def inspect
+      "«#{self}»"
+    end
+
+    def reducible?
+      false
+    end
+  end
+
+  class Add < Struct.new(:left, :right)
+    def to_s
+      "#{left} + #{right}"
+    end
+
+    def inspect
+      "«#{self}»"
+    end
+
+    def reducible?
+      true
+    end
+
+    def reduce
+      if left.reducible?
+        Add(left.reduce, right)
+      elsif right.reducible?
+        Add(left, right.reduce)
+      else
+        Number.new(left.value + right.value)
+      end
+    end
+  end
+
+  class Multiply < Struct.new(:left, :right)
+    def to_s
+      "#{left} * #{right}"
+    end
+
+    def inspect
+      "«#{self}»"
+    end
+
+    def reducible?
+      true
+    end
+
+    def reduce
+      if left.reducible?
+        Multiply(left.reduce, right)
+      elsif right.reducible?
+        Multiply(left, right.reduce)
+      else
+        Number.new(left.value * right.value)
+      end
+    end
+  end
+
+  # Module functions to make shit pretty
   module_function
 
   def Number(value)
-    OpenStruct.new(value: value)
+    Number.new(value)
   end
 
   def Add(left, right)
-    OpenStruct.new(left: left, right: right)
+    Add.new(left, right)
   end
 
   def Multiply(left, right)
-    OpenStruct.new(left: left, right: right)
+    Multiply.new(left, right)
   end
 end
--- test/ast_nodes_test.rb
+++ test/ast_nodes_test.rb
@@ -9,4 +9,59 @@ describe 'Understanding Computation' do
       Multiply(Number(3), Number(4))
     )
   end
+
+  it 'prints inspect with guillemets' do
+    assert_output(/«5»/) {
+      puts Number(5).inspect
+    }
+  end
+
+  describe '#reducible' do
+    describe Number do
+      it 'is not reducible' do
+        refute Number(5).reducible?
+      end
+    end
+
+    describe Add do
+      it 'is reducible' do
+        assert Add(Number(1), Number(2)).reducible?
+      end
+    end
+
+    describe Multiply do
+      it 'is reducible' do
+        assert Multiply(Number(1), Number(2)).reducible?
+      end
+    end
+  end
+
+  describe '#reduce' do
+    before do
+      @node = Add(
+        Multiply(Number(1), Number(2)),
+        Multiply(Number(3), Number(4))
+      )
+    end
+
+    it 'reduces the left side first' do
+      reduced = Add(
+        Number(2),
+        Multiply(Number(3), Number(4))
+      )
+
+      assert_equal reduced, @node.reduce
+    end
+
+    it 'reduces both sides after two steps' do
+      reduced = Add(
+        Number(2),
+        Number(12)
+      )
+
+      @node = @node.reduce
+
+      assert_equal reduced, @node.reduce
+    end
+  end
 end
