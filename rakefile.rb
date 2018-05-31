37e67202c53ee85bc5c0b46484814e0d0b8c951b
Brian Kung <brian@callmekung.com>
2018-05-31 13:09:00 -0500

    Less exuberant notifications

--- rakefile.rb
+++ rakefile.rb
@@ -3,8 +3,8 @@ task default: %w[test]
 task :test do
   begin
     ruby "test/test.rb"
-    `terminal-notifier -message '✅✅✅✅✅' && say 'finished'`
+    `terminal-notifier -message '✅' && say 'finished'`
   rescue
-    `terminal-notifier -message '❗️❗️❗️❗️❗️' && say 'something broke'`
+    `terminal-notifier -message '❗️' && say 'something broke'`
   end
 end
